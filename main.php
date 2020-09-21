7f8539490a39b55eae36e3ad8426d83521a1129b
Thomas HALLOT <thallot@e1r5p10.42.fr>
2020-09-21 14:57:23 +0200

    ff

--- main.php
+++ main.php
@@ -2,16 +2,21 @@
 
 include 'parsing.php';
 
-class Calc
+class Computor
 {
 
   public $maxDegree;
 
-  public $number;
+  public $numbers;
 
-  public function __construct($parser)
+  public $parser;
+
+  public function __construct($argv)
   {
-    $this->numbers = $parser->numbers;
+    $this->parser = new Parser($argv);
+    $this->parser->parse();
+    $this->numbers = $this->parser->numbers;
+
   }
 
   public function getReducedForm()
@@ -19,7 +24,7 @@ class Calc
     $reduced = 'Reduced form:';
     foreach ($this->numbers as $key => $value) {
       if ($value != 0)
-        $reduced .= sprintf(' %s * X^%s', $value, $key);
+        $reduced .= sprintf(' %s %s * X^%s', $value > 0 && $key > 0 ? '+' : '', $value, $key);
     }
 
     return $reduced . PHP_EOL;
@@ -48,10 +53,51 @@ class Calc
     return ($this->numbers[1] * $this->numbers[1]) - (4 * $this->numbers[0] * $this->numbers[2]);
   }
 
+  public function calcResult()
+  {
+    $delta = $this->calcDelta();
+    $x = $y = null;
+
+    if ($delta > 0)
+    {
+      $x = ($this->numbers[1] * -1 - sqrt($delta)) / ( 2 * $this->numbers[2]);
+      $y = ($this->numbers[1] * -1 + sqrt($delta)) / ( 2 * $this->numbers[2]);
+    }
+    else if ($delta == 0)
+    {
+      $x = (-$this->numbers[1]) / ( 2 * $this->numbers[2]);
+    }
+
+    return ['X' =>$x, 'Y' => $y, 'DELTA' => $delta];
+  }
+
+  public function printResult()
+  {
+    $result = $this->calcResult();
+    $x = $result['X'];
+    $y = $result['Y'];
+    $delta = $result['DELTA'];
+
+    if ($delta < 0)
+    {
+      printf("Discriminant is strickly negativ (%d), no real solution \n", $delta);
+    }
+    if ($delta > 0)
+    {
+      printf("Discriminant is strickly positiv (%d), there are two solutions \n", $delta);
+      printf("X1: %s \n", $x);
+      printf("X2: %s", $y);
+    }
+    else
+    {
+      printf("Discriminant is null, there are one solution \n");
+      printf("X: %s", $x);
+    }
+  }
+
 }
 
-$parser = new Parser($argv);
-$parser->parse();
-$calc = new Calc($parser);
-$calc->printInfo();
-var_dump($calc->calcDelta());
+
+$computor = new Computor($argv);
+$computor->printInfo();
+$computor->printResult();
